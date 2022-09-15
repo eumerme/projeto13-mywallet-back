88b8e2f6118e4b5a36e0e0142c786125630a4742
eumerme <euthaisetau@gmail.com>
2022-09-15 11:41:39 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import { router } from './routes/index.js';
+import express from "express";
+import cors from "cors";
+import { router } from "./routes/index.js";
 
 const app = express();
 app.use(cors());
@@ -8,4 +8,6 @@ app.use(express.json());
 
 app.use(router);
 
-app.listen(5000, () => console.log('Listening on port 5000'));
+app.listen(process.env.PORT, () =>
+	console.log(`Listening on port ${process.env.PORT}`)
+);
