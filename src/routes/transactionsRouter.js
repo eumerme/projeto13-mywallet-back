f27f020cad4cec0af6f16c005355a27076352852
eumerme <euthaisetau@gmail.com>
2023-01-20 21:15:11 -0300

    feat: update transaction

--- src/routes/transactionsRouter.js
+++ src/routes/transactionsRouter.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { tokenValidation, validateBody, validateParams } from "../middlewares/index.js";
-import { postTransaction, getTransactions, deleteTransaction } from "../controllers/index.js";
-import { transactionBody, transactionParams } from "../schemas/index.js";
+import { postTransaction, getTransactions, deleteTransaction, updateTransaction } from "../controllers/index.js";
+import { createTransactionBody, transactionParams, updateTransactionBody } from "../schemas/index.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter
 	.all("/*", tokenValidation)
-	.post("/transactions", validateBody(transactionBody), postTransaction)
+	.post("/transactions", validateBody(createTransactionBody), postTransaction)
 	.get("/transactions", getTransactions)
+	.patch("/transactions/:id", validateBody(updateTransactionBody), validateParams(transactionParams), updateTransaction)
 	.delete("/transactions/:id", validateParams(transactionParams), deleteTransaction);
 
 export { transactionsRouter };
--- src/schemas/transactionsSchema.js
+++ src/schemas/transactionsSchema.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
-const transactionBody = Joi.object().keys({
+const createTransactionBody = Joi.object().keys({
 	email: Joi.string().trim().email().required(),
 	date: Joi.string().trim().required(),
 	value: Joi.number().required(),
@@ -8,8 +8,14 @@ const transactionBody = Joi.object().keys({
 	type: Joi.string().valid("credit", "debit").required(),
 });
 
+const updateTransactionBody = Joi.object().keys({
+	date: Joi.string().trim().required(),
+	value: Joi.number().required(),
+	description: Joi.string().trim().required(),
+});
+
 const transactionParams = Joi.object().keys({
 	id: Joi.string().trim().required(),
 });
 
-export { transactionBody, transactionParams };
+export { createTransactionBody, updateTransactionBody, transactionParams };
